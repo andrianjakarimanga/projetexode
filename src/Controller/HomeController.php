8e60ea838c09a727cfed85b9d19aa53f9ccd63f0
julien93130 <112167112+julien93130@users.noreply.github.com>
2022-09-08 10:07:09 +0200

    maj bdd 8/09
    ajout de login

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    public function index(): JsonResponse
+    public function index():JsonResponse
     {
         return $this->json([
             'message' => 'Welcome to your new controller!',
