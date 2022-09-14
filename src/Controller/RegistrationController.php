665613d76c61c8637b2727f164a9b29bd091ed78
julien93130 <112167112+julien93130@users.noreply.github.com>
2022-09-14 15:36:54 +0200

    page register
    ajout de l integration register

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -19,6 +19,7 @@ class RegistrationController extends AbstractController
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager ,AvatarRepository $AvatarRepository ): Response
     {
         $user = new User();
+
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
 
@@ -30,6 +31,8 @@ class RegistrationController extends AbstractController
                     $form->get('plainPassword')->getData()
                 )
             );
+            $user->setRang(0);
+            $user->setScore(0);
 
             $entityManager->persist($user);
             $entityManager->flush();
