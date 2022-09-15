8132d96d646da49513885c8a4e3e316f1fb22a7a
julien93130 <112167112+julien93130@users.noreply.github.com>
2022-09-15 16:15:12 +0200

    ajout de page register win , lost
    ajout des pages win et lost
    page register complete sans cgv

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -22,26 +22,48 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('pseudo', TextType::class, [  'attr' => [
-                'placeholder' => 'form.contact.email.placeholder',
+            ->add('pseudo', TextType::class, [  
+                'attr' => [
+                    'placeholder' => 'Pseudo',
+                ],
+            ])
+
+            ->add('email', TextType::class, [  'attr' => [
+                'placeholder' => 'Email',
             ],
                 
             ])
-            ->add('email')
-            ->add('agreeTerms', CheckboxType::class, [
+
+           
+           
+            /*->add('agreeTerms', CheckboxType::class, [
+
+              
                 'mapped' => false,
                 'constraints' => [
                     new IsTrue([
                         'message' => 'You should agree to our terms.',
-                      
+
                     ]),
                 ],
-            ])
+
+            ])*/
+
+
+            
+
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
+              
+                
+
+
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
+                'attr' => [
+                    'autocomplete' => 'new-password',
+                    'placeholder' => 'Password',
+                ],
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Please enter a password',
